c39556918fb627ad914af558934a2bef779cd92b
Luugallego17 <luna.gallego@ucb.edu.bo>
2025-05-27 18:08:35 -0400

    Ejervcicio 1 realizado

--- tests/exercise1_test.cpp
+++ tests/exercise1_test.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
 
-void exercise_1(const std::string& a, const std::string& b, const std::string& c) {
-    std::string min = a;
+void exercise_1(const string& a, const string& b, const string& c) {
+    string min = a;
     if (b < min) min = b;
     if (c < min) min = c;
-    std::cout << min;
+    cout << min;
 }
